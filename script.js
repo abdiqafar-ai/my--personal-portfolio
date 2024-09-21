3bf65ea5b1feae69b63e1b75d5a559a1deb8f525
abdiqafar-ai <iabdiqafar24@gmail.com>
2024-09-21 21:27:31 +0300

    connect index.html to style.css to script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "";
 document.querySelector("body").appendChild(h2);
+
+document.getElementById('my form').addEventListener('submit',function(e){
+e.preventDefault();
+let Name=document.getElementById(Name).value;
+let Email=document.getElementById('Email').value;
+let Message=document.getElementById('Message').value
+if (Name==="||Email==="||Message===""){
+    alert('Please fill in the all field')
+}else{
+    alert('Form submitted successfully')
+}
+
+})
