3674d0fa77919c44ed86747d3f52ff2812f6e509
abdiqafar-ai <iabdiqafar24@gmail.com>
2024-09-23 08:26:28 +0300

    commit

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "";
-document.querySelector("body").appendChild(h2);
-
-document.getElementById('my form').addEventListener('submit',function(e){
-e.preventDefault();
-let Name=document.getElementById(Name).value;
-let Email=document.getElementById('Email').value;
-let Message=document.getElementById('Message').value
-if (Name==="||Email==="||Message===""){
-    alert('Please fill in the all field')
-}else{
-    alert('Form submitted successfully')
+document.querySelector("body").appendChild(h2)
+let Home;
+document.getElementById('Home').onclick=function (){
+    Home=document.getElementById
 }
-
-})
+function addingEventListener(){
+    const input = document.getElementById('My form');
+    input.addEventListener('submit', function() {
+      alert('submitted!');
+    });
+    }
+   
